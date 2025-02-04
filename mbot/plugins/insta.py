15f13ada610778b69f234243ad02dc8c56b34b18
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-04 11:37:12 +0530

    Update insta.py

--- mbot/plugins/insta.py
+++ mbot/plugins/insta.py
@@ -23,9 +23,9 @@ async def link_handler(Mbot, message):
         url= link.replace("instagram.com","ddinstagram.com")
         url=url.replace("==","%3D%3D")
         if url.endswith("="):
-           dump_file=await message.reply_video(url[:-1],caption="Thank you for using - @Rkgroup_bot)
+           dump_file=await message.reply_video(url[:-1],caption="Thank you for using - @Rkgroup_bot")
         else:
-            dump_file=await message.reply_video(url,caption="Thank you for using - @Rkgroup_bot)
+            dump_file=await message.reply_video(url,caption="Thank you for using - @Rkgroup_bot")
         if 'dump_file' in locals():
            await dump_file.forward(DUMP_GROUP)
         await m.delete()
