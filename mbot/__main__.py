9eef2e2a60f6f1d4dcfc1d313a821383c09b562d
Rkgroup <110547855+RKgroupkg@users.noreply.github.com>
2025-02-04 08:27:38 +0530

    Update __main__.py
    Added keep alive

--- mbot/__main__.py
+++ mbot/__main__.py
@@ -24,6 +24,8 @@ SOFTWARE.
 from mbot import Mbot
 from os import sys,mkdir,path
 
+from keep_alive import keep_alive
+keep_alive()
 if __name__ == "__main__":
     if not path.exists("cache"):
         mkdir("cache")
